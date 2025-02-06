23c18c101ee43984daf267029a47b3fecc8f746d
Juan Barbosa <juanbarbosavella@gmail.com>
2025-02-06 11:50:00 -0500

    Renamed the .env.local file to .env to ensure Prisma reads environment variables correctly.

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home(){
   return(
     <div>
       Link a pagina dibujar
-      <Link href={"/draw"}>Dibujar</Link>
+      <Link className="bg-blue-500 hover:bg-blue-600 cursor-pointer py-1 px-2 rounded-md text-white" href={"/draw"}>Dibujar</Link>
     </div>
   )
 }
